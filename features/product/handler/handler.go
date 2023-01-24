6cabfbb6a9a6da96fefc29ef0e5fa530438cae2c
Fauzi Sofyan <fauzilax@gmail.com>
2023-01-24 13:20:17 +0700

    Build: Config helper and structure folder from prodcut, sorry forget change branch to product revisi

--- features/product/handler/handler.go
+++ features/product/handler/handler.go
@@ -1,1 +1,42 @@
 package handler
+
+import (
+	"ecommerce/features/product"
+
+	"github.com/labstack/echo/v4"
+)
+
+type handlerController struct {
+	srv product.ProductService
+}
+
+func New(ps product.ProductService) product.ProductHandler {
+	return &handlerController{
+		srv: ps,
+	}
+}
+
+// AddProduct implements product.ProductHandler
+func (*handlerController) AddProduct() echo.HandlerFunc {
+	panic("unimplemented")
+}
+
+// AllProduct implements product.ProductHandler
+func (*handlerController) AllProduct() echo.HandlerFunc {
+	panic("unimplemented")
+}
+
+// Delete implements product.ProductHandler
+func (*handlerController) Delete() echo.HandlerFunc {
+	panic("unimplemented")
+}
+
+// EditProduct implements product.ProductHandler
+func (*handlerController) EditProduct() echo.HandlerFunc {
+	panic("unimplemented")
+}
+
+// ProductDetail implements product.ProductHandler
+func (*handlerController) ProductDetail() echo.HandlerFunc {
+	panic("unimplemented")
+}
