c16b7a36921b2937c2560c336210b9387f7447b8
kgriffinh <griffinh29@gmail.com>
2023-01-24 15:59:43 +0800

    chore: add user query

--- features/user/data/query.go
+++ features/user/data/query.go
@@ -1,1 +1,91 @@
 package data
+
+import (
+	"ecommerce/features/user"
+	"errors"
+	"log"
+
+	"gorm.io/gorm"
+)
+
+type userQuery struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) user.UserData {
+	return &userQuery{
+		db: db,
+	}
+}
+
+func (uq *userQuery) Login(email string) (user.Core, error) {
+	res := User{}
+
+	if err := uq.db.Where("email = ?", email).First(&res).Error; err != nil {
+		log.Println("login query error", err.Error())
+		return user.Core{}, errors.New("data not found")
+	}
+
+	return ToCore(res), nil
+}
+
+func (uq *userQuery) Register(newUser user.Core) (user.Core, error) {
+	cnv := CoreToData(newUser)
+	err := uq.db.Create(&cnv).Error
+
+	if err != nil {
+		return user.Core{}, err
+	}
+
+	newUser.ID = cnv.ID
+	newUser.Password = ""
+	return newUser, nil
+}
+
+func (uq *userQuery) Profile(id uint) (user.Core, error) {
+	res := User{}
+
+	if err := uq.db.Where("id = ?", id).First(&res).Error; err != nil {
+		log.Println("Get By ID query error", err.Error())
+		return user.Core{}, err
+	}
+
+	return ToCore(res), nil
+}
+
+func (uq *userQuery) Update(id uint, updateData user.Core) (user.Core, error) {
+	cnv := CoreToData(updateData)
+	qry := uq.db.Model(&User{}).Where("id = ?", id).Updates(&cnv)
+
+	affrows := qry.RowsAffected
+	if affrows == 0 {
+		log.Println("no rows affected")
+		return user.Core{}, errors.New("no data updated")
+	}
+
+	err := qry.Error
+	if err != nil {
+		log.Println("update user query error", err.Error())
+		return user.Core{}, err
+	}
+
+	return ToCore(cnv), nil
+}
+
+func (uq *userQuery) Delete(id uint) error {
+	qry := uq.db.Delete(&User{}, id)
+	err := qry.Error
+
+	affrows := qry.RowsAffected
+	if affrows == 0 {
+		log.Println("no rows affected")
+		return errors.New("no data deleted")
+	}
+
+	if err != nil {
+		log.Println("delete query error")
+		return errors.New("cannot delete data")
+	}
+
+	return nil
+}
