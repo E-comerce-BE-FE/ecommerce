61015eee6c3178d9e1645c6d3a6f36b523e7f6e1
kgriffinh <griffinh29@gmail.com>
2023-01-24 23:24:45 +0800

    chore: add description in requestbody product

--- features/product/handler/request.go
+++ features/product/handler/request.go
@@ -9,12 +9,14 @@ type AddProductRequest struct {
 	ProductName string `json:"product_name" form:"product_name"`
 	Price       int    `json:"price" form:"price"`
 	Stock       int    `json:"stock" form:"stock"`
+	Description string `json:"description" form:"description"`
 	FileHeader  multipart.FileHeader
 }
 type EditProductRequest struct {
 	ProductName string `json:"product_name" form:"product_name"`
 	Price       int    `json:"price" form:"price"`
 	Stock       int    `json:"stock" form:"stock"`
+	Description string `json:"description" form:"description"`
 	FileHeader  multipart.FileHeader
 }
 
@@ -26,11 +28,13 @@ func RequestToCore(dataProduct interface{}) *product.Core {
 		res.ProductName = cnv.ProductName
 		res.Price = cnv.Price
 		res.Stock = cnv.Stock
+		res.Description = cnv.Description
 	case EditProductRequest:
 		cnv := dataProduct.(EditProductRequest)
 		res.ProductName = cnv.ProductName
 		res.Price = cnv.Price
 		res.Stock = cnv.Stock
+		res.Description = cnv.Description
 	default:
 		return nil
 	}
