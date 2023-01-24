da3fe7176247b444d5a957049fb9d7ee0ccd29bf
kgriffinh <griffinh29@gmail.com>
2023-01-24 16:18:21 +0800

    chore: add user handler

--- features/user/handler/handler.go
+++ features/user/handler/handler.go
@@ -1,1 +1,103 @@
 package handler
+
+import (
+	"ecommerce/features/user"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type userControll struct {
+	srv user.UserService
+}
+
+func New(srv user.UserService) user.UserHandler {
+	return &userControll{
+		srv: srv,
+	}
+}
+
+func (uc *userControll) Login() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		input := LoginRequest{}
+		if err := c.Bind(&input); err != nil {
+			return c.JSON(http.StatusBadRequest, "input format incorrect")
+		}
+
+		token, res, err := uc.srv.Login(input.Email, input.Password)
+		if err != nil {
+			return c.JSON(PrintErrorResponse(err.Error()))
+		}
+
+		return c.JSON(PrintSuccessReponse(http.StatusOK, "success login", ToResponse(res), token))
+	}
+}
+func (uc *userControll) Register() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		input := RegisterRequest{}
+		if err := c.Bind(&input); err != nil {
+			return c.JSON(http.StatusBadRequest, "input format incorrect")
+		}
+
+		res, err := uc.srv.Register(*ReqToCore(input))
+		if err != nil {
+			return c.JSON(PrintErrorResponse(err.Error()))
+		}
+
+		return c.JSON(PrintSuccessReponse(http.StatusCreated, "success register data", ToResponse(res)))
+	}
+}
+func (uc *userControll) Profile() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		token := c.Get("user")
+
+		res, err := uc.srv.Profile(token)
+		if err != nil {
+			return c.JSON(PrintErrorResponse(err.Error()))
+		}
+
+		return c.JSON(PrintSuccessReponse(http.StatusOK, "success show profile", PPToResponse(res)))
+	}
+}
+
+func (uc *userControll) Update() echo.HandlerFunc {
+	return func(c echo.Context) error {
+
+		formHeader, err := c.FormFile("file")
+		if err != nil {
+			return c.JSON(
+				http.StatusInternalServerError,
+				helper.MediaDto{
+					StatusCode: http.StatusInternalServerError,
+					Message:    "error",
+					Data:       &echo.Map{"data": "Select a file to upload"},
+				})
+		}
+
+		token := c.Get("user")
+		input := UpdateRequest{}
+		if err := c.Bind(&input); err != nil {
+			return c.JSON(http.StatusBadRequest, "input format incorrect")
+		}
+
+		res, err := uc.srv.Update(token, *formHeader, *ReqToCore(input))
+		if err != nil {
+			return c.JSON(PrintErrorResponse(err.Error()))
+		}
+
+		return c.JSON(PrintSuccessReponse(http.StatusOK, "success update profile", PPToResponse(res)))
+	}
+}
+
+func (uc *userControll) Delete() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		token := c.Get("user")
+
+		err := uc.srv.Delete(token)
+		if err != nil {
+			return c.JSON(PrintErrorResponse(err.Error()))
+		}
+
+		return c.JSON(PrintSuccessReponse(http.StatusOK, "success delete profile", err))
+	}
+}
