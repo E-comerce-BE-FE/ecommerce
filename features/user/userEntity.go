d7bc0ff7bd436153d30728eb4d62aaafbb63b1c0
kgriffinh <griffinh29@gmail.com>
2023-01-24 15:19:28 +0800

    chore: add entity user

--- features/user/userEntity.go
+++ features/user/userEntity.go
@@ -1,1 +1,41 @@
 package user
+
+import (
+	"mime/multipart"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Core struct {
+	ID             uint
+	Name           string
+	Email          string
+	Phone          string
+	Address        string
+	Password       string
+	Profilepicture string
+}
+
+type UserHandler interface {
+	Login() echo.HandlerFunc
+	Register() echo.HandlerFunc
+	Profile() echo.HandlerFunc
+	Delete() echo.HandlerFunc
+	Update() echo.HandlerFunc
+}
+
+type UserService interface {
+	Login(email, password string) (string, Core, error)
+	Register(newUser Core) (Core, error)
+	Profile(token interface{}) (Core, error)
+	Update(token interface{}, fileData multipart.FileHeader, updateData Core) (Core, error)
+	Delete(token interface{}) error
+}
+
+type UserData interface {
+	Login(email string) (Core, error)
+	Register(newUser Core) (Core, error)
+	Profile(id uint) (Core, error)
+	Update(id uint, updateData Core) (Core, error)
+	Delete(id uint) error
+}
