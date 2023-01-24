1e3a4bda101daa22666346ae38428fc948da7ea0
kgriffinh <griffinh29@gmail.com>
2023-01-24 14:30:25 +0800

    chore: add service user

--- features/user/services/service.go
+++ features/user/services/service.go
@@ -1,1 +1,146 @@
 package services
+
+import (
+	"ecommerce/features/user"
+	"errors"
+	"log"
+	"mime/multipart"
+	"strings"
+
+	"github.com/go-playground/validator"
+	"github.com/golang-jwt/jwt"
+)
+
+type userUseCase struct {
+	qry user.UserData
+	vld *validator.Validate
+}
+
+func New(ud user.UserData) user.userService {
+	return &userUseCase{
+		qry: ud,
+		vld: validator.New(),
+	}
+}
+
+func (uuc *userUseCase) Login(email, password string) (string, user.Core, error) {
+	res, err := uuc.qry.Login(email)
+	if err != nil {
+		msg := ""
+		if strings.Contains(err.Error(), "not found") {
+			msg = "data not found"
+		} else {
+			msg = "server error"
+		}
+		return "", user.Core{}, errors.New(msg)
+	}
+
+	if err := helper.CheckPassword(res.Password, password); err != nil {
+		log.Println("login compare", err.Error())
+		return "", user.Core{}, errors.New("password not matched")
+	}
+
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["userID"] = res.ID
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	useToken, _ := token.SignedString([]byte(config.JWT_KEY))
+
+	return useToken, res, nil
+
+}
+
+func (uuc *userUseCase) Register(newUser user.Core) (user.Core, error) {
+	hashed, err := helper.GeneratePassword(newUser.Password)
+	if err != nil {
+		log.Println("bcrypt error ", err.Error())
+		return user.Core{}, errors.New("password process error")
+	}
+
+	newUser.Password = string(hashed)
+	res, err := uuc.qry.Register(newUser)
+
+	if err != nil {
+		msg := ""
+		if strings.Contains(err.Error(), "duplicated") {
+			msg = "data already used"
+		} else {
+			msg = "server error"
+		}
+		return user.Core{}, errors.New(msg)
+	}
+
+	return res, nil
+}
+
+func (uuc *userUseCase) Profile(token interface{}) (user.Core, error) {
+	id := helper.ExtractToken(token)
+	if id <= 0 {
+		return user.Core{}, errors.New("data not found")
+	}
+
+	res, err := uuc.qry.Profile(uint(id))
+	if err != nil {
+		msg := ""
+		if strings.Contains(err.Error(), "not found") {
+			msg = "data not found"
+		} else {
+			msg = "server error"
+		}
+		return user.Core{}, errors.New(msg)
+	}
+
+	return res, nil
+}
+
+func (uuc *userUseCase) Update(token interface{}, fileData multipart.FileHeader, updateData user.Core) (user.Core, error) {
+	id := helper.ExtractToken(token)
+	if id <= 0 {
+		return user.Core{}, errors.New("data not found")
+	}
+
+	formFile, err := fileData.Open()
+	if err != nil {
+		return user.Core{}, errors.New("incorrect input")
+	}
+
+	uploadUrl, err := helper.NewMediaUpload().FileUpload(helper.File{File: formFile})
+	if err != nil {
+		return user.Core{}, errors.New("incorrect input")
+	}
+
+	updateData.Profilepicture = uploadUrl
+
+	res, err := uuc.qry.Update(uint(id), updateData)
+	if err != nil {
+		msg := ""
+		if strings.Contains(err.Error(), "not found") {
+			msg = "data not found"
+		} else {
+			msg = "server error"
+		}
+		return user.Core{}, errors.New(msg)
+	}
+
+	return res, nil
+}
+
+func (uuc *userUseCase) Delete(token interface{}) error {
+	id := helper.ExtractToken(token)
+	if id <= 0 {
+		return errors.New("data not found")
+	}
+
+	err := uuc.qry.Delete(uint(id))
+	if err != nil {
+		msg := ""
+		if strings.Contains(err.Error(), "not found") {
+			msg = "data not found"
+		} else {
+			msg = "server error"
+		}
+		return errors.New(msg)
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -5,11 +5,16 @@ go 1.19
 require github.com/labstack/echo/v4 v4.10.0
 
 require (
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
 )
 
 require (
+	github.com/go-playground/validator v9.31.0+incompatible
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
