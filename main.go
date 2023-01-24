4617e3f2f5aaa6e5622ad9d6246f654d3fa0d541
Fauzi Sofyan <fauzilax@gmail.com>
2023-01-24 19:00:11 +0700

    CHORE: add some endpoint for user

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	prdData "ecommerce/features/product/data"
 	prdHdl "ecommerce/features/product/handler"
 	prdSrv "ecommerce/features/product/services"
+	usrData "ecommerce/features/user/data"
+	usrHdl "ecommerce/features/user/handler"
+	usrSrv "ecommerce/features/user/services"
 	"log"
 
 	"github.com/labstack/echo/v4"
@@ -19,14 +22,26 @@ func main() {
 	// gunakan migrate
 	config.Migrate(db)
 
+	uData := usrData.New(db)
+	uSrv := usrSrv.New(uData)
+	uHdl := usrHdl.New(uSrv)
+
 	pData := prdData.New(db)
 	pSrv := prdSrv.New(pData)
 	pHDL := prdHdl.New((pSrv))
+
 	e.Pre(middleware.RemoveTrailingSlash())
 	e.Use(middleware.CORS())
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "method=${method}, uri=${uri}, status=${status}, error=${error}\n",
 	}))
+
+	// User
+	e.POST("/register", uHdl.Register())
+	e.POST("/login", uHdl.Login())
+	e.PUT("/users", uHdl.Update(), middleware.JWT([]byte(config.JWTKey)))
+	e.DELETE("/users", uHdl.Delete(), middleware.JWT([]byte(config.JWTKey)))
+	e.GET("/users", uHdl.Profile(), middleware.JWT([]byte(config.JWTKey)))
 
 	// Product
 	e.GET("/product", pHDL.AllProduct())
