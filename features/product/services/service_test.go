28f6fc227e92c1cb7002a3325a05c269316bdb8a
kgriffinh <griffinh29@gmail.com>
2023-01-25 17:51:38 +0800

    chore: unit testing product

--- features/product/services/service_test.go
+++ features/product/services/service_test.go
@@ -5,6 +5,10 @@ import (
 	"ecommerce/helper"
 	"ecommerce/mocks"
 	"errors"
+	"log"
+	"mime/multipart"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/golang-jwt/jwt"
@@ -13,17 +17,60 @@ import (
 
 func TestAddProduct(t *testing.T) {
 	data := mocks.NewProductData(t)
-	input := product.Core{ID: 1, ProductName: "Kitkat", Price: 10000, Stock: 5}
-	resData := product.Core{ID: 1, ProductName: "Kitkat", Price: 10000, Stock: 5}
+	filePath := filepath.Join("..", "..", "..", "ERD.png")
+	// imageFalse, _ := os.Open(filePath)
+	// imageFalseCnv := &multipart.FileHeader{
+	// 	Filename: imageFalse.Name(),
+	// }
+	imageTrue, err := os.Open(filePath)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	imageTrueCnv := &multipart.FileHeader{
+		Filename: imageTrue.Name(),
+	}
+
+	input := product.Core{ID: 1, ProductName: "Kitkat", Price: 10000, Stock: 5, ProductImage: imageTrueCnv.Filename}
+	resData := product.Core{ID: 1, ProductName: "Kitkat", Price: 10000, Stock: 5, ProductImage: imageTrueCnv.Filename}
 
 	t.Run("Success add product", func(t *testing.T) {
-		data.On("AddProduct", 1, input).Return(resData, nil).Once()
+		data.On("AddProduct", uint(1), input).Return(resData, nil).Once()
 
-		// srv := New(data)
-		// _, token := helper.GenerateToken(1)
-		// useToken := token.(*jwt.Token)
-		// useToken.Valid = true
-		// res, err := srv.AddProduct(useToken,)
+		srv := New(data)
+		_, token := helper.GenerateToken(1)
+		useToken := token.(*jwt.Token)
+		useToken.Valid = true
+		res, err := srv.AddProduct(useToken, *imageTrueCnv, input)
+		assert.Nil(t, err)
+		assert.Equal(t, resData.ID, res.ID)
+		data.AssertExpectations(t)
+	})
+
+	t.Run("Trouble in server", func(t *testing.T) {
+		data.On("AddProduct", uint(1), input).Return(product.Core{}, errors.New("server error")).Once()
+
+		srv := New(data)
+		_, token := helper.GenerateToken(1)
+		useToken := token.(*jwt.Token)
+		useToken.Valid = true
+		res, err := srv.AddProduct(useToken, *imageTrueCnv, input)
+		assert.NotNil(t, err)
+		assert.NotEqual(t, resData.ID, res.ID)
+		data.AssertExpectations(t)
+	})
+
+	t.Run("JWT not valid", func(t *testing.T) {
+		data.On("AddProduct", uint(1), input).Return(product.Core{}, errors.New("jwt invalid")).Once()
+
+		srv := New(data)
+		_, token := helper.GenerateToken(1)
+		useToken := token.(*jwt.Token)
+		useToken.Valid = true
+		res, err := srv.AddProduct(useToken, *imageTrueCnv, input)
+		assert.NotNil(t, err)
+		assert.NotEqual(t, resData.ID, res.ID)
+		assert.ErrorContains(t, err, "error")
+		data.AssertExpectations(t)
 	})
 }
 
@@ -76,6 +123,19 @@ func TestDelete(t *testing.T) {
 		data.AssertExpectations(t)
 	})
 
+	t.Run("Data not found", func(t *testing.T) {
+		data.On("Delete", uint(1), uint(2)).Return(errors.New("not found")).Once()
+
+		srv := New(data)
+		_, token := helper.GenerateToken(1)
+		useToken := token.(*jwt.Token)
+		useToken.Valid = true
+		err := srv.Delete(useToken, 2)
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "error")
+		data.AssertExpectations(t)
+	})
+
 	t.Run("Trouble in server", func(t *testing.T) {
 		data.On("Delete", uint(1), uint(1)).Return(errors.New("server error")).Once()
 
@@ -92,17 +152,61 @@ func TestDelete(t *testing.T) {
 
 func TestEditProduct(t *testing.T) {
 	data := mocks.NewProductData(t)
-	input := product.Core{ID: 1, ProductName: "Kitkat", Price: 10000, Stock: 3}
-	resData := product.Core{ID: 1, ProductName: "Tehbotol", Price: 5000, Stock: 1}
+	filePath := filepath.Join("..", "..", "..", "ERD.png")
+	// imageFalse, _ := os.Open(filePath)
+	// imageFalseCnv := &multipart.FileHeader{
+	// 	Filename: imageFalse.Name(),
+	// }
+	imageTrue, err := os.Open(filePath)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	imageTrueCnv := &multipart.FileHeader{
+		Filename: imageTrue.Name(),
+	}
+
+	input := product.Core{ID: 1, ProductName: "Kitkat", Price: 10000, Stock: 3, ProductImage: "ERD.png"}
+	resData := product.Core{ID: 1, ProductName: "Tehbotol", Price: 5000, Stock: 1, ProductImage: imageTrueCnv.Filename}
 
 	t.Run("Success update data", func(t *testing.T) {
 		data.On("EditProduct", uint(1), uint(1), input).Return(resData, nil).Once()
 
-		// srv := New(data)
-		// _, token := helper.GenerateToken(1)
-		// useToken := token.(*jwt.Token)
-		// useToken.Valid = true
-		// res, err := srv.EditProduct(useToken)
+		srv := New(data)
+		_, token := helper.GenerateToken(1)
+		useToken := token.(*jwt.Token)
+		useToken.Valid = true
+		res, err := srv.EditProduct(useToken, *imageTrueCnv, 1, input)
+		assert.Nil(t, err)
+		assert.NotEqual(t, input.ProductName, res.ProductName)
+		data.AssertExpectations(t)
+	})
+
+	t.Run("Trouble in server", func(t *testing.T) {
+		data.On("EditProduct", uint(1), uint(1), input).Return(product.Core{}, errors.New("server error")).Once()
+
+		srv := New(data)
+		_, token := helper.GenerateToken(1)
+		useToken := token.(*jwt.Token)
+		useToken.Valid = true
+		res, err := srv.EditProduct(useToken, *imageTrueCnv, 1, input)
+		assert.NotNil(t, err)
+		assert.Equal(t, uint(0), res.ID)
+		assert.ErrorContains(t, err, "error")
+		data.AssertExpectations(t)
+	})
+
+	t.Run("JWT not valid", func(t *testing.T) {
+		data.On("AddProduct", uint(1), input).Return(product.Core{}, errors.New("jwt invalid")).Once()
+
+		srv := New(data)
+		_, token := helper.GenerateToken(1)
+		useToken := token.(*jwt.Token)
+		useToken.Valid = true
+		res, err := srv.AddProduct(useToken, *imageTrueCnv, input)
+		assert.NotNil(t, err)
+		assert.NotEqual(t, resData.ID, res.ID)
+		assert.ErrorContains(t, err, "error")
+		data.AssertExpectations(t)
 	})
 }
 
